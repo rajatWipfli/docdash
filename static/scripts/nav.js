c9ecce176cbd5f8b0c62c38d245d13604605dafc
rajatwipfli <rajat.parashar@wipfli.com>
2022-07-15 17:52:56 +0530

    Add extra UI

--- static/scripts/nav.js
+++ static/scripts/nav.js
@@ -1,12 +1,44 @@
 function scrollToNavItem() {
-    var path = window.location.href.split('/').pop().replace(/\.html/, '');
-    document.querySelectorAll('nav a').forEach(function(link) {
-      var href = link.attributes.href.value.replace(/\.html/, '');
-      if (path === href) {
-        link.scrollIntoView({block: 'center'});
-        return;
-      }
-    })
-  }
+  var path = window.location.href.split('/').pop().replace(/\.html/, '');
+  document.querySelectorAll('nav a').forEach(function (link) {
+    var href = link.attributes.href.value.replace(/\.html/, '');
+    if (path === href) {
+      link.scrollIntoView({block: 'center'});
+      return;
+    }
+  })
+}
 
-  scrollToNavItem();
+function getActiveNav(allNavs) {
+  return Array.from(allNavs).find((navA) => {
+    return navA.href === location.href;
+  });
+}
+
+function activateSectionWithActiveLink() {
+  const allNavs = document.querySelectorAll('nav a');
+  const activeLink = getActiveNav(allNavs);
+  const parentSection = activeLink.closest('details');
+  if (!parentSection) {
+    return;
+  }
+  parentSection.toggleAttribute('open', true);
+  allNavs.forEach(function (link) {
+    const li = link.closest('li');
+    if (li) {
+      li.classList.remove('active');
+    }
+  });
+  const activeLinkLi = activeLink.closest('li');
+  if (activeLinkLi) {
+    activeLinkLi.classList.add('active');
+  }
+}
+function delayNavActivation() {
+  setTimeout(activateSectionWithActiveLink, 500)
+}
+
+scrollToNavItem();
+
+window.addEventListener('hashchange', activateSectionWithActiveLink);
+window.addEventListener('load', delayNavActivation);
